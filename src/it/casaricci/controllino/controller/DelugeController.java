a1a2f138911889816199105c94b5f8069889f627
Daniele Ricci <daniele.athome@gmail.com>
2012-02-24 18:56:43 +0100

    Basic Deluge torrent daemon controller
    Signed-off-by: Daniele Ricci <daniele.athome@gmail.com>

---
+++ src/it/casaricci/controllino/controller/DelugeController.java
@@ -0,0 +1,67 @@
+package it.casaricci.controllino.controller;
+
+import it.casaricci.controllino.ConnectorService;
+
+
+/**
+ * Deluge daemon controller.
+ * @author Daniele Ricci
+ */
+public class DelugeController extends SysVInitController {
+	private UpdateShellListener mUpdateListener = new UpdateShellListener();
+
+    public DelugeController(ConnectorService connector) {
+        super(connector, "deluge-daemon");
+    }
+
+    @Override
+    public CharSequence getServiceName() {
+        return "Deluge torrent daemon";
+    }
+
+    @Override
+    public CharSequence getServiceDescription() {
+        return "Deluge torrent daemon using System V init script.";
+    }
+
+    @Override
+    public void update() {
+    	update(mUpdateListener, true);
+    }
+
+    private final class UpdateShellListener implements ShellExecuteListener {
+
+        @Override
+        public void onError(ShellController ctrl, Throwable e) {
+            // TODO
+            setStatus(Status.STATUS_ERROR);
+        }
+
+        @Override
+        public void onExecuteFinish(ShellController ctrl, int exitStatus) {
+            // TODO init script is not capable of checking daemon status
+            setStatus(Status.STATUS_UNKNOWN);
+        }
+    }
+
+    @Override
+    public void start() {
+        start(new StartStopShellListener(mUpdateListener));
+    }
+
+    @Override
+    public void stop() {
+        stop(new StartStopShellListener(mUpdateListener));
+    }
+
+    @Override
+    public void restart() {
+        restart(new StartStopShellListener(mUpdateListener));
+    }
+
+    @Override
+    public void reload() {
+        reload(new StartStopShellListener(mUpdateListener));
+    }
+
+}
